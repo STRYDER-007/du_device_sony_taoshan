45428e593e1a5d29bd777a3e7b167d2971e13d2d
Mahesh <mahesh140494@gmail.com>
2016-09-02 15:30:00 +0530

    taoshan: DU-N bringup

--- du.mk
+++ du.mk
@@ -15,10 +15,10 @@
 #
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common DU stuff
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/sony/taoshan/device.mk)
@@ -29,7 +29,7 @@ TARGET_SCREEN_WIDTH := 480
 
 # Device identifications
 PRODUCT_DEVICE := taoshan
-PRODUCT_NAME := cm_taoshan
+PRODUCT_NAME := du_taoshan
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia L
