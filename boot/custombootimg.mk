25e4d4abddf0c18bcc314787e52c0f1b8acbdb21
Mahesh <mahesh140494@gmail.com>
2016-09-14 19:47:44 +0000

    taoshan: Re-write custombootimg.mk and add prebuilt TWRP recovery ramdisk
    Change-Id: I4c9ad24bb5f2d532ffa385e1fac627b65e8f4f56

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -1,55 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
-DEVICE_BOOTDIR := device/sony/taoshan/boot
-DEVICE_LOGORLE := $(DEVICE_BOOTDIR)/logo.rle
-INITSONY := $(PRODUCT_OUT)/utilities/init_sony
-PREBUILT_TOYBOX := device/sony/taoshan/recovery/toybox
-
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
-recovery_uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.cpio
-recovery_uncompressed_device_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery-device.cpio
-$(recovery_uncompressed_device_ramdisk): $(MKBOOTFS) \
-		$(INTERNAL_RECOVERYIMAGE_FILES) \
-		$(recovery_initrc) $(recovery_sepolicy) $(recovery_kernel) \
-		$(INSTALLED_2NDBOOTLOADER_TARGET) \
-		$(recovery_build_prop) $(recovery_resource_deps) $(recovery_root_deps) \
-		$(recovery_fstab) \
-		$(RECOVERY_INSTALL_OTA_KEYS) \
-		$(INTERNAL_BOOTIMAGE_FILES)
-	$(call build-recoveryramdisk)
-	@echo -e ${CL_CYN}"----- Making uncompressed recovery amdisk ------"${CL_RST}
-	$(hide) cp $(DEVICE_LOGORLE) $(TARGET_RECOVERY_ROOT_OUT)/
-	$(hide) $(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
-	$(hide) rm -f $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_device_ramdisk) $(recovery_uncompressed_ramdisk)
-
-recovery_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.img
-$(recovery_ramdisk): $(MINIGZIP) \
-		$(recovery_uncompressed_device_ramdisk)
-	@echo -e ${CL_CYN}"----- Making compressed recovery ramdisk ------"${CL_RST}
-	$(hide) $(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $@
+DEVICE_BOOTDIR := device/sony/taoshan/boot/
+PREBUILT_RAMDISK := device/sony/taoshan/recovery/ramdisk-recovery.cpio
+PREBUILT_TOYBOX := device/sony/taoshan/recovery/toybox
+INITSONY := $(PRODUCT_OUT)/utilities/init_sony
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
-		$(uncompressed_ramdisk) \
-		$(recovery_uncompressed_device_ramdisk) \
-		$(INSTALLED_RAMDISK_TARGET) \
-		$(INITSONY) \
-		$(PREBUILT_TOYBOX) \
-		$(PRODUCT_OUT)/utilities/keycheck \
-		$(MKBOOTIMG) $(MINIGZIP) \
-		$(INTERNAL_BOOTIMAGE_FILES)
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(PREBUILT_RAMDISK) $(INSTALLED_RAMDISK_TARGET) $(INITSONY) $(PREBUILT_TOYBOX) $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Boot image: $@")
 
-	@echo -e ${CL_CYN}"----- Making boot image ------"${CL_RST}
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) cp -a $(PRODUCT_OUT)/root $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
 
-	$(hide) cp $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
-	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
+	$(hide) cp $(DEVICE_BOOTDIR)/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
+	$(hide) cp $(PREBUILT_RAMDISK) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PREBUILT_TOYBOX) $(PRODUCT_OUT)/combinedroot/sbin/toybox_init
 
@@ -61,15 +30,12 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_BOOTIMAGE_TARGET)
-	
-	$(hide) ln -f $(INSTALLED_BOOTIMAGE_TARGET) $(PRODUCT_OUT)/boot.elf 
-	$(call pretty,"Made boot image: $@")
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk) \
-		$(recovery_kernel)
-	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(recovery_ramdisk) \
+	$(recovery_kernel)
 	$(call build-recoveryimage-target, $@)
+	@echo ----- Making recovery image ------
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
-	$(call pretty,"Made recovery image: $@")
+	@echo ----- Made recovery image -------- $@
