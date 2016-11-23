58dd54545432fe57f03653050e5b18e3b07d2dc9
Mahesh <mahesh140494@gmail.com>
2016-11-23 14:21:44 +0000

    taoshan: AICP-fy tree
    Change-Id: Ica8e71519823c5367ae2971e08089d1c46ecbbad

--- aicp_taoshan.mk
+++ aicp_taoshan.mk
@@ -15,10 +15,10 @@
 #
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common AICP stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/sony/taoshan/device.mk)
@@ -29,7 +29,7 @@ TARGET_SCREEN_WIDTH := 480
 
 # Device identifications
 PRODUCT_DEVICE := taoshan
-PRODUCT_NAME := cm_taoshan
+PRODUCT_NAME := aicp_taoshan
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia L
